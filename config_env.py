f391ab4a0265f7b555a870f224ae28a3a9d92334
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-11-21 01:23:32 -0500

    add newton config

---
+++ config_env.py
@@ -0,0 +1,14 @@
+# This file configures platform, path and datasets
+import os
+
+PROCESS_UNIT = "gpu" # or "cpu"
+
+# Path to caffe
+CAFFE_DIR = os.path.join("/home", "workspace", "caffe")
+
+# Path to the datasets directory
+# IMAGENET_DIR = os.path.join("/groups", "arcc", "shared", "datasets", "ImageNet2012nonpub")
+IMAGENET_DIR = os.path.join("/home", "datasets", "tiny-imagenet-200")
+PLACES205_DIR = os.path.join("/home", "datasets", "Places205")
+PASCAL_VOC_2007_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2007")
+PASCAL_VOC_2012_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2012")
