0e225eb82810875d4b2704ae8b89cd89f2f9ddb4
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-11-21 11:17:45 -0500

    fixed tiny imagenet path

--- config_env.py
+++ config_env.py
@@ -8,7 +8,7 @@ CAFFE_DIR = os.path.join("/home", "workspace", "caffe")
 
 # Path to the datasets directory
 # IMAGENET_DIR = os.path.join("/groups", "arcc", "shared", "datasets", "ImageNet2012nonpub")
-IMAGENET_DIR = os.path.join("/home", "datasets", "tiny-imagenet-200")
+IMAGENET_DIR = os.path.join("/home", "cap6614.student6", "datasets", "tiny-imagenet-200")
 PLACES205_DIR = os.path.join("/home", "datasets", "Places205")
 PASCAL_VOC_2007_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2007")
 PASCAL_VOC_2012_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2012")
