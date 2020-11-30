399b7d67900e6bd7ff6d2b1c2bf9aed7858de5b1
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-11-29 22:37:38 -0500

    add miniplaces path

--- config_env.py
+++ config_env.py
@@ -9,6 +9,6 @@ CAFFE_DIR = os.path.join("/home", "workspace", "caffe")
 # Path to the datasets directory
 # IMAGENET_DIR = os.path.join("/groups", "arcc", "shared", "datasets", "ImageNet2012nonpub")
 IMAGENET_DIR = os.path.join("/home", "cap6614.student6", "datasets", "tiny-imagenet-200")
-PLACES205_DIR = os.path.join("/home", "datasets", "Places205")
+PLACES205_DIR = os.path.join("/home", "cap6614.student6", "datasets", "miniplaces")
 PASCAL_VOC_2007_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2007")
 PASCAL_VOC_2012_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2012")
